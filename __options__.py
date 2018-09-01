9b4d881d344336c1dada5933621f9a767d88fc5f
Mikael Lindqvist <li.mikael@gmail.com>
2018-09-01 11:40:05 +0200

    __options__ should not be version controled

--- __options__.py
+++
@@ -1,7 +0,0 @@
-SLICER="/home/micke/.local/opt/Slic3rPE-1.40.1.linux64-full-201807051330.AppImage"
-SLICER_INI_FILES=[
-	"/home/micke/.Slic3rPE/printer/PrusaMK3.ini",
-	"/home/micke/.Slic3rPE/print/0.30mmPROTOTYPE.ini",
-	"/home/micke/.Slic3rPE/filament/MyPLA.ini"
-]
-
